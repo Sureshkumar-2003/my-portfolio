a7fa4cdd592eecf552d4798de3944de498982b1b
Sureshkumar-2003 <sureshmoni01@gmail.com>
2024-10-07 20:02:04 -0700

    my portfolio website

---
+++ main.js
@@ -0,0 +1,28 @@
+var type = new Typed('.input'
+    , {
+        strings:["Front-End Developer", "Creative Designer"],
+        typeSpeed:150,
+        backSpeed:150,
+        backDealy:2000,
+        loop:true
+
+    }
+)
+
+
+// const text =document.querySelector(".sec-text");
+
+// const texrLoad= ()=>{
+//     setTimeout(()=>{
+//        text.textContent= "FRONT-END DEVELOPER";
+//     },0);
+
+//     setTimeout(()=>{
+//         text.textContent= "CREATIVE DESIGNER";
+//      },4000);
+
+
+//      texrLoad();
+
+    
+// };
