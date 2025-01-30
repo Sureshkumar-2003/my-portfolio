1c8a9e1a1bcfa72cacb32ce8d8dbf20df305c461
Sureshkumar-2003 <sureshmoni01@gmail.com>
2025-01-30 03:43:16 -0800

    change

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
-var type = new Typed('.input'
-    , {
-        strings:["Front-End Developer", "Creative Designer"],
-        typeSpeed:150,
-        backSpeed:150,
-        backDealy:2000,
-        loop:true
-
-    }
-)
 
 
-// const text =document.querySelector(".sec-text");
+// nav bar
 
-// const texrLoad= ()=>{
-//     setTimeout(()=>{
-//        text.textContent= "FRONT-END DEVELOPER";
-//     },0);
+const list= document.querySelector('.nav-ul');
 
-//     setTimeout(()=>{
-//         text.textContent= "CREATIVE DESIGNER";
-//      },4000);
+const menuBar= document.querySelector('.menu-bar');
+
+const closeBtn= document.querySelector('#close-btn');
 
 
-//      texrLoad();
 
-    
-// };
+// click event And class list add 
+
+
+ menuBar.addEventListener('click',()=>{
+     list.classList.add("active")
+
+ });
+
+//  close btn
+
+ closeBtn.addEventListener('click',()=>{
+    list.classList.remove("active")
+
+});
+
+
+// view card
+
+// const image=document.querySelectorAll('.image');
+// const view=document.querySelector('.over-view');
+// const remove=document.querySelector('#view-remove')
+// const main=document.querySelector('#main-img');
+// console.log(image);
+
+// image.forEach((img)=>(
+//     img.addEventListener('click',()=>{
+//         view.classList.add('view-active')
+//         main.src=img.src;
+//     })
+// ));
+
+
+// remove.addEventListener('click',()=>{
+
+//     view.classList.remove('view-active')
+// });
+
+
+
+// var type = new Typed('.input'
+//     , {
+//         strings:["Front-End Developer", "Creative Designer"],
+//         typeSpeed:150,
+//         backSpeed:150,
+//         backDealy:2000,
+//         loop:true
+
+//     }
+// )
+
+
